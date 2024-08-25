ee29a0bc6ec16dda69710e9c22e9e1c8c52e6a78
ankit <ankitdhindhwal@gmail.com>
2024-08-25 16:45:20 +0530

    serilog

---
+++ WebApplication5/WebApplication5/DependencyEx.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace WebApplication5
+{
+    public static class DependencyEx
+    {
+        public static IServiceCollection addSerilog1(this IServiceCollection services)
+        {
+            Log.Logger = new LoggerConfiguration().
+            MinimumLevel.Debug()
+           .WriteTo.Console()
+           .WriteTo.File("logs/myapp.txt")
+           .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+           .MinimumLevel.Override("Microsoft.Extensions.Hosting", LogEventLevel.Information)
+           .MinimumLevel.Override("Microsoft.Hosting", LogEventLevel.Information)
+           .CreateLogger();
+
+            Log.Information("This is serilog logging");
+            services.AddSerilog();
+            return services;
+        }
+
+    }    
+}
---
+++ WebApplication5/WebApplication5/Program.cs
@@ -0,0 +1,28 @@
+using Serilog;
+using Serilog.Events;
+using WebApplication5;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.addSerilog1();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+Log.Information("Starting Host");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
